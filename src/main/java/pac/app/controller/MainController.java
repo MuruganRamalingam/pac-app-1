7228d3f73d295cc82a101cf2fb78e6027872ef49
Murugan Ramalingam <murugan.r-external@tcs.com>
2022-07-15 19:51:21 +0530

    update by murugan

--- src/main/java/pac/app/controller/MainController.java
+++ src/main/java/pac/app/controller/MainController.java
@@ -142,8 +142,59 @@ public class MainController {
     @Post("/js")
     @Produces(MediaType.APPLICATION_JSON)
     public String saveEvent(@Body String body) {
+        LOG.info("Local Test1");
+        LOG.info(body);
+        body="{'jan1':'1234567ABCDEF','rank':'1'}";
+        String[] param=body.split(",");
+        String[] param1=param[0].split(":");
+        String jan=param1[1].replace("\"","").replace("'","").trim();
+        LOG.info(jan);
 
-        return body;
+        String rank="";
+        //SONArray jsonArray = new JSONArray(body);
+        amazonDynamoDBClient = AmazonDynamoDBClientBuilder.standard()
+                .withCredentials(new DefaultAWSCredentialsProviderChain())
+                .withRegion(Regions.US_EAST_1).build();
+
+        LOG.info("Local Test2");
+
+        HashMap<String, Condition> scanFilter = new HashMap<>();
+
+        Condition condition = new Condition().withComparisonOperator(ComparisonOperator.EQ.toString())
+                .withAttributeValueList(new AttributeValue().withS(jan));
+
+        scanFilter.put("jan", condition);
+        ScanRequest scanRequest = new ScanRequest("pac_all").withScanFilter(scanFilter);
+        ScanResult scanResult = amazonDynamoDBClient.scan(scanRequest);
+        List<java.util.Map<String, AttributeValue>> aa = scanResult.getItems();
+        LOG.info(aa.size());
+        AttributeValue cc= new AttributeValue();
+
+        String base_point="";
+        String base_promotionDesc="";
+        for (int i = 0; i < aa.size(); i++) {
+            java.util.Map<String, AttributeValue> bb = aa.get(i);
+            Iterator<String> iterator = bb.keySet().iterator();
+            while (iterator.hasNext()) {
+                String key = iterator.next();
+                cc = bb.get(key);
+                if(key.equals("PromotionDesc"))
+                {
+                    base_promotionDesc = cc.toString().substring(4);
+                    base_promotionDesc =base_promotionDesc.substring(0, base_promotionDesc.length() - 2);
+                }
+                if(key.equals("point"))
+                {
+                    base_point = cc.toString().substring(4);
+                    base_point =base_point.substring(0, base_point.length() - 2);
+                }
+                LOG.info(key);
+                LOG.info(cc.toString());
+            }
+        }
+        String s =String.valueOf(cc);
+        return  "{\"point\":\"" + base_point + "\",\"PromotionDesc\":\"" + base_promotionDesc + "\"}";
+
     }
 
     @Get("/test")
