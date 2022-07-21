9a76a8fd51671e66e3f74635cc19a50f6397d0c0
Murugan Ramalingam <murugan.r-external@tcs.com>
2022-07-21 10:53:21 +0530

    update by murugan

--- src/main/java/pac/app/controller/MainController.java
+++ src/main/java/pac/app/controller/MainController.java
@@ -217,7 +217,8 @@ public class MainController {
                     base_point = base_point.substring(0, base_point.length() - 2);
                 }
                 if (key.equals("rank")) {
-                    base_rank = cc.toString();
+                    base_rank = cc.toString().substring(4);
+                    base_rank = base_rank.substring(0, base_rank.length() - 2);
                 }
                 LOG.info(key);
                 LOG.info(cc.toString());
@@ -252,6 +253,7 @@ public class MainController {
         String base_promotionCode = "";
         String base_rewardCode = "";
         String base_promotionDesc = "";
+        int base_point;
         for (int i = 0; i < aa.size(); i++) {
             java.util.Map<String, AttributeValue> bb = aa.get(i);
             Iterator<String> iterator = bb.keySet().iterator();
@@ -271,45 +273,5 @@ public class MainController {
             // return "{\"Member rank\":\"" +jan + "\",\"All Points\":\"" +all_points + "\",\"PromotionCode\":\"" + base_promotionCode + "\",\"Promotion Desc\":\""+base_promotionDesc+ "\", \"Store Code\":\""+ base_maStoreCode+"\",\"RewardCode\":\""+base_rewardCode+"\"}";
         }
         return "{\"MasterStroreCode\":\"" + base_masterStoreCode + "\",\"MaStoreCode\":\"" + base_maStoreCode + "\",\"PromotionCode\":\"" + base_promotionCode + "\",\"RewardCode\":\"" + base_rewardCode + "\"}";
-    }
-    @Get("/pe003")
-    public String getAllEvent(@Body String body1) {
-        LOG.info("LocalTest8");
-        body1 = "jan:1234567ABCDEF";
-        LOG.info(body1);
-        String [] s11 = body1.split(":");
-        String jan1 = s11[1];
-        LOG.info(jan1+ "::" +s11[1].length());
-        amazonDynamoDBClient = AmazonDynamoDBClientBuilder.standard()
-                .withCredentials(new DefaultAWSCredentialsProviderChain())
-                .withRegion(Regions.US_EAST_1).build();
-        HashMap<String, Condition> scanFilter = new HashMap<>();
-        Condition condition = new Condition().withComparisonOperator(ComparisonOperator.EQ.toString())
-                .withAttributeValueList(new AttributeValue().withS(jan1));
-        scanFilter.put("jan", condition);
-        ScanRequest sr1 = new ScanRequest("pac_all").withScanFilter(scanFilter);
-        ScanResult sre = amazonDynamoDBClient.scan(sr1);
-        List<java.util.Map<String, AttributeValue>> aa = sre.getItems();
-        LOG.info(aa.size());
-        AttributeValue cc = new AttributeValue();
-        LOG.info(jan1);
-        String base_masterStoreCode1 = "";
-        String base_maStoreCode1= "";
-        String base_promotionCode1 = "";
-        String base_rewardCode1="";
-        String base_promotionDesc1 = "";
-        base_masterStoreCode1 = jan1.substring(0,4);
-        base_maStoreCode1 = jan1.substring(5,6);
-        base_promotionCode1 = jan1.substring(6,10);
-        base_rewardCode1 = jan1.substring(10);
-        ResultSetMetaData rsMetaData = rs.getMetaData();
-        int count = rsMetaData.getColumeCount();
-        for (int i = 1; i < count; i++) {
-            LOG.info(rsMetaData.getColumename(i));
-        }
-        LOG.info(cc.toString());
-        LOG.info(base_masterStoreCode1);
-        return "{\"MasterStroreCode\":\"" + base_masterStoreCode1 + "\",\"MaStoreCode\":\"" + base_maStoreCode1 + "\",\"PromotionCode\":\"" + base_promotionCode1 + "\",\"RewardCode\":\""+base_rewardCode1 +"\"}";
-        // return "{\"Member rank\":\"" +jan + "\",\"All Points\":\"" +all_points + "\",\"PromotionCode\":\"" + base_promotionCode + "\",\"Promotion Desc\":\""+base_promotionDesc+ "\", \"Store Code\":\""+ base_maStoreCode+"\",\"RewardCode\":\""+base_rewardCode+"\"}";
     }
 }
