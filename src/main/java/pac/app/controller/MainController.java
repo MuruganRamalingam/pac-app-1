7f1a279ba07703548e688bacbdb83b937fb984ca
Murugan Ramalingam <murugan.r-external@tcs.com>
2022-07-26 09:33:52 +0530

    update by murugan

--- src/main/java/pac/app/controller/MainController.java
+++ src/main/java/pac/app/controller/MainController.java
@@ -302,12 +302,12 @@ public class MainController {
                     jan_code=String.valueOf(item.get("jan"));
                     LOG.info(jan_code);
                     LOG.info(item.get("PromotionDesc"));
-                    promotion_desc=String.valueOf(item.get("PromotionDesc");
+                    promotion_desc=String.valueOf(item.get("PromotionDesc"));
                     LOG.info(promotion_desc);
                     String p=item.get("point").toString();
                     point_value = Integer.parseInt(p);
                     LOG.info(point_value);
-                    LOG.info(item.get("sdt"));
+                    LOG.info(item.get("sdt")); 
                     sdt = String.valueOf(item.get("sdt"));
                     LOG.info(sdt);
                     LOG.info(item.get("type"));
