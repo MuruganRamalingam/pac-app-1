382a024bbe8f498110d77fbb467f0b34a231ef3f
Murugan Ramalingam <murugan.r-external@tcs.com>
2022-07-26 09:25:02 +0530

    update by murugan

--- src/main/java/pac/app/controller/MainController.java
+++ src/main/java/pac/app/controller/MainController.java
@@ -299,10 +299,10 @@ public class MainController {
                 LOG.info(item.keySet());
                 LOG.info(item.values());
                 if (jan.contains(base_promotionCode)) {
-                    jan_code=String.valueOf(item.get("jan").toString());
+                    jan_code=String.valueOf(item.get("jan"));
                     LOG.info(jan_code);
                     LOG.info(item.get("PromotionDesc"));
-                    promotion_desc=String.valueOf(item.get("PromotionDesc").toString());
+                    promotion_desc=String.valueOf(item.get("PromotionDesc");
                     LOG.info(promotion_desc);
                     String p=item.get("point").toString();
                     point_value = Integer.parseInt(p);
@@ -311,7 +311,7 @@ public class MainController {
                     sdt = String.valueOf(item.get("sdt"));
                     LOG.info(sdt);
                     LOG.info(item.get("type"));
-                    type=String.valueOf(item.get("type").toString());
+                    type=String.valueOf(item.get("type"));
                     LOG.info(type);
                 }
                 else {
