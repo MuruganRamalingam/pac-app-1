440b027610fed5bd758ba83deae7283723396f35
Murugan Ramalingam <murugan.r-external@tcs.com>
2022-08-04 19:20:22 +0530

    update by murugan

--- src/main/java/pac/app/controller/AwsController.java
+++ src/main/java/pac/app/controller/AwsController.java
@@ -50,7 +50,7 @@ public class AwsController {
     public String saveEvent(@Body String body) {
         LOG.info("Local Test1");
         LOG.info(body);
-//        body="{'jan1':'1234567ABCDEF','jan1':'1234567ABCDEF','jan1':'1234567ABCDEF','rank':'1'}";
+//        body="{'jan1':'1234567ABCDEF','rank':'1'}";
         String[] param = body.split(",");
         String[] param1 = param[0].split(":");
         String jan = param1[1].replace("\"", "").replace("'", "").trim();
