7b8255bbe6c6ecd6e25bae00eccbb3f2cbd8c747
Murugan Ramalingam <murugan.r-external@tcs.com>
2022-08-05 11:01:13 +0530

    update by murugan

--- src/main/java/pac/app/controller/MainController.java
+++ src/main/java/pac/app/controller/MainController.java
@@ -163,8 +163,8 @@ public class MainController {
                 String key = iterator.next();
                 cc = bb.get(key);
                 if (key.equals("jan")) {
-                    base_masterStoreCode = jan.substring(0, 4);
-                    base_maStoreCode = jan.substring(4, 6);
+                    base_masterStoreCode = jan.substring(0, 5);
+                    base_maStoreCode = jan.substring(5, 6);
                     base_promotionCode = jan.substring(6, 10);
                     base_rewardCode = jan.substring(10);
                 }
